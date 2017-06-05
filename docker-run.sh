782177701c326e6e56e739727d3f3b84560a29eb
ahuh <ahuh@free.fr>
2017-06-05 15:31:21 +0200

    Update docker scripts with default values

--- docker-run.sh
+++ docker-run.sh
@@ -8,23 +8,81 @@
 # 'docker stop' command 
 # =======================================================================================
 
-# Parameters
+# ------------------------------------------------------
+# Custom parameters
+
+export DNS_1=8.8.8.8
+export DNS_2=8.8.4.4
+
 export V_CONFIG=/shares/P2P/tools/subzero
 export V_WORKINGFOLDER=/shares/Data/Series
+
 export E_PUID=500
 export E_PGID=1000
 
-if [[ $# != 2 ]] ; then
+# ------------------------------------------------------
+# Common parameters
+
+export CONTAINER_NAME=subzero
+export IMAGE_NAME_1=arm-subzero
+export IMAGE_NAME_2=ahuh/arm-subzero
+export IMAGE_NAME=
+
+if [[ "$1" = "h" ]] || [[ "$1" = "help" ]] || [[ "$1" = "-h" ]] || [[ "$1" = "-help" ]] || [[ "$1" = "--h" ]] || [[ "$1" = "--help" ]]; then
+    echo 'Run a Docker container.'
     echo ''
-    echo 'Usage: docker-run.sh <docker-container-name> <docker-image-name>'
+    echo 'Usage:'    
+    echo '  docker-run.sh [CONTAINER_NAME] [IMAGE_NAME]'
+    echo '  docker-run.sh h | help | -h | -help | --h | --help'
+    echo ''
+    echo 'Options:'
+    echo "  CONTAINER_NAME  Name of the container [default: ${CONTAINER_NAME}]"
+    echo "  IMAGE_NAME      Name of the image [default: ${IMAGE_NAME_1} (if exists), ${IMAGE_NAME_2} (otherwise)]"
     echo ''
     exit 1
 fi
 
-export CONTAINER_NAME=$1
-export IMAGE_NAME=$2
+if [[ $1 ]]; then
+	CONTAINER_NAME=$1
+else
+	echo "Using default container name: ${CONTAINER_NAME}"
+fi
+if [[ $2 ]]; then
+	IMAGE_NAME=$2
+else
+	if [[ $(docker images | awk '{ print $1,$3 }' | grep -E "^${IMAGE_NAME_1}\s" | wc -l) != 0 ]] ; then
+		IMAGE_NAME=${IMAGE_NAME_1}
+	else
+		IMAGE_NAME=${IMAGE_NAME_2}
+	fi
+	echo "Using default image name: ${IMAGE_NAME}"
+fi
 
+# ------------------------------------------------------
+# Common commands
 
+if [[ $(docker ps -f name=${CONTAINER_NAME} -f status=running | grep ${CONTAINER_NAME} | wc -l) != 0 ]] ; then
+	# Container already running: stop it
+	echo "Stop running container: ${CONTAINER_NAME}"
+	docker stop ${CONTAINER_NAME}
+	RESULT=$?
+	if [[ ${RESULT} != 0 ]] ; then
+		exit 1
+	fi
+fi
 
-# Commands
-docker run --name ${CONTAINER_NAME} --restart=always -d -v ${V_CONFIG}:/config -v ${V_WORKINGFOLDER}:/workingfolder -v /etc/localtime:/etc/localtime:ro -e "PUID=${E_PUID}" -e "PGID=${E_PGID}" ${IMAGE_NAME}
+if [[ $(docker ps -a -f name=${CONTAINER_NAME} | grep ${CONTAINER_NAME} | wc -l) != 0 ]] ; then
+	# Container already exists: remove it
+	echo "Remove existing container: ${CONTAINER_NAME}"
+	docker rm ${CONTAINER_NAME}
+	RESULT=$?
+	if [[ ${RESULT} != 0 ]] ; then
+		exit 1
+	fi
+fi
+
+# ------------------------------------------------------
+# Custom commands
+
+echo "Run container: ${CONTAINER_NAME}"
+docker run --name ${CONTAINER_NAME} --restart=always --dns=${DNS_1} --dns=${DNS_2} -d -v ${V_CONFIG}:/config -v ${V_WORKINGFOLDER}:/workingfolder -v /etc/localtime:/etc/localtime:ro -e "PUID=${E_PUID}" -e "PGID=${E_PGID}" ${IMAGE_NAME}
